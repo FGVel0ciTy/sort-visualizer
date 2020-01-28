9c09fecfd6c46c7b854deaf41e0fb8eb0ea04081
KV Le <32186663+FGVel0ciTy@users.noreply.github.com>
2020-01-27 17:08:54 -0800

    Added default values

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ blue = (0, 0, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-display_width = 800
-display_height = 800
-grid_height = 100
-grid_width = 100
-tile_width = display_width // grid_width
-tile_height = display_height // grid_height
+default_display_width = 800
+default_display_height = 800
+default_grid_width = 100
+default_grid_height = 100
+tile_width = default_display_width // default_grid_width
+tile_height = default_display_height // default_grid_height
 
 os.environ['SDL_VIDEO_CENTERED'] = "0"
 pygame.init()
-screen = pygame.display.set_mode((display_width, display_height))
+screen = pygame.display.set_mode((default_display_width, default_display_height))
 screen.fill(white)
 
 
@@ -38,7 +38,7 @@ class Tile:
 
 
 class Grid:
-    def __init__(self, grid_width=100, grid_height=100):
+    def __init__(self, grid_width=100, grid_height=100, display_width=800, display_height=800):
         self.grid = grid = numpy.empty((grid_width, grid_height), object)
         for x in range(grid_width):
             for y in range(grid_height):
