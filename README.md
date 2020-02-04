b6cff7dc84d63b2317aa82746b0acc333f64ca05
KV Le <32186663+FGVel0ciTy@users.noreply.github.com>
2020-02-03 17:04:17 -0800

    Insertion Sort is now in

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sort-visualizer
-This project visualizes the sorting of colors using various sorting algorithms (WIP)
+This project visualizes the sorting of colors using various sorting algorithms.
+
+Currently only insertion sort is in and if you want the visual to go faster, change default_step_time to something lower.
+
+## Controls
+Reset Grid - F5
+
+Start Algorithm - Return or Enter
--- main.py
+++ main.py
@@ -14,12 +14,15 @@ blue = [0, 0, 255]
 white = [255, 255, 255]
 black = [0, 0, 0]
 
-display_width = 450
-display_height = 450
-grid_width = 90
-grid_height = 90
+display_width = 600
+display_height = 300
+grid_width = 30
+grid_height = 15
 tile_width = display_width // grid_width
 tile_height = display_height // grid_height
+
+default_sleep_time = .1
+sleep_time = default_sleep_time
 
 os.environ['SDL_VIDEO_CENTERED'] = "0"
 pygame.init()
@@ -39,6 +42,9 @@ class Tile:
         r, g, b = self.color
         self.hue = colorsys.rgb_to_hsv(r, g, b)[0]
 
+    def __repr__(self):
+        return f"Tile at {self.x}, {self.y} with {self.color} color"
+
     def update_color(self, color):
         self.color = color
         pygame.draw.rect(screen, color, (self.x * tile_width, self.y * tile_height, tile_width, tile_height))
@@ -47,28 +53,19 @@ class Tile:
         self.hue = colorsys.rgb_to_hsv(r, g, b)[0]
         return self.color
 
-    def update(self):
-        pygame.draw.rect(screen, self.color, (self.x * tile_width, self.y * tile_height, tile_width, tile_height))
-        pygame.display.update((self.x * tile_width, self.y * tile_height, tile_width, tile_height))
-        r, g, b = self.color
-        self.hue = colorsys.rgb_to_hsv(r, g, b)[0]
-
 
 grid = numpy.empty((grid_width, grid_height), object)
 
 colors = []
 for i in range(3):
-    temp_color = [0, 0, 0]
-    temp_color[i] = 255
-    for j in range(0, 256, 51):
-        for k in range(0, 256, 51):
-            other = [j, k]
-            for m in range(3):
-                if m != i:
-                    temp_color[m] = other.pop()
-            if temp_color not in colors:
-                colors.append(temp_color[:])
-colors.remove(white)
+    for j in range(3):
+        temp_color = [0, 0, 0]
+        temp_color[i] = 255
+        if j != i:
+            for k in range(0, 256, 51):
+                temp_color[j] = k
+                if temp_color not in colors:
+                    colors.append(temp_color[:])
 print(len(colors), colors)
 
 
@@ -77,14 +74,24 @@ def initialize():
         temp = colors[:]
         for x in range(grid_width):
             grid[x, y] = Tile(x, y, temp.pop(random.randint(0, len(temp) - 1)))
-            print(grid[x, y].hue)
+            print(grid[x, y])
 
 
 def insertion_sort():
     for y in range(grid_height):
-        current_x = 0
-        current_min_x = 0
-        pass
+        for x in range(grid_width):
+            min_x = x
+            for next_x in range(x + 1, grid_width):
+                if grid[min_x, y].hue > grid[next_x, y].hue:
+                    min_x = next_x
+            swap(grid[x, y], grid[min_x, y])
+
+
+def swap(tile1, tile2):
+    temp = tile2.color[:]
+    tile2.update_color(tile1.color)
+    time.sleep(sleep_time)
+    tile1.update_color(temp)
 
 
 while True:
